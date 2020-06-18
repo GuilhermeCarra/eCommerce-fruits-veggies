e06e59fcf2d073a94d07c2c8c913fffde2121566
guilherme-carra <gui_gc@hotmail.com>
2020-06-18 21:46:43 +0200

    Changing how is printend the products on the html

--- src/js/script.js
+++ src/js/script.js
@@ -17,33 +17,24 @@ printProducts();
 ----------------------------------------------------- */
 
 function printProducts(filter) {
-    $(".productsLine").remove();
-    let products = [...shop.Products];
-    if (filter != undefined) products = filter;
-    let row = '<div class="row mb-4 productsLine"></div>';
-    let emptyCol = '<div class="col-2"></div>';
-    let lines = Math.ceil(products.length / 4);
-
-    for (let n = 0; n < lines; n++) {
-        $("#footer").before($(row).append(
-            emptyCol,
-            makeCard(products),
-            emptyCol
-        ))
+    $("#products_list").empty();
+    let products;
+    if (filter != undefined) {
+        products = filter;
+    } else {
+        products = [...shop.Products];
     }
-}
-
-function makeCard(products) {
-    let emptyCol = '<div class="col-2"></div>';
+    let emptyCol = '<div class="col-3 pb-4"></div>';
     let card = '<div class="card"><div>';
     let img = '<img src="" class="card-img-top" data-toggle="modal" data-target="#product_details">';
     let cardBody = '<div class="card-body"></div>';
     let cardTitle = '<h5 class="card-title"></h5>';
     let cardText = '<p class="card-text"></p>';
-    let cardProduct = [];
-    for (let i = 0; i <4; i++) {
-        if (products.length > 0) {
-            cardProduct[i] = $(emptyCol).append(
+    let productsNumber = products.length;
+
+    for (let n = 0; n < productsNumber; n++) {
+        $("#products_list").append(
+            $(emptyCol).append(
                 $(card).append(
                     $(img).attr("src",products[0].img).data("idProduct",products[0].id).click(showProduct),
                     $(cardBody).append(
@@ -52,10 +43,9 @@ function makeCard(products) {
                     )
                 )
             )
-            products.shift();
-        }
+        )
+        products.shift();
     }
-    return cardProduct;
 }
 
 $("#categoryFilter").change(function() {
