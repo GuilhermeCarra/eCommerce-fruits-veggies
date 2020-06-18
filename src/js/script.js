ede6837b3660a3e7dc4c28cd34131f05fcbcd8a9
sayeed <sayeed0209@gamil.com>
2020-06-18 10:18:08 +0200

    some changes has been made

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,64 @@
-if (localStorage.getItem("shop") == null) {
-    var shop = [];
-    localStorage.setItem("shop", shop);
+if (localStorage.getItem('shop') == null) {
+  var shop = [];
+  localStorage.setItem('shop', shop);
 } else {
-    var shop = JSON.parse(localStorage.getItem("shop"));
+  var shop = JSON.parse(localStorage.getItem('shop'));
 }
 let products = shop.Products;
 
 printProducts();
 
 function printProducts() {
-    let row = '<div class="row mb-4"></div>';
-    let emptyCol = '<div class="col-2"></div>';
-    let lines = Math.ceil(products.length / 4);
+  let row = '<div class="row mb-4"></div>';
+  let emptyCol = '<div class="col-2"></div>';
+  let lines = Math.ceil(products.length / 4);
 
-    for (let n = 0; n < lines; n++) {
-        $("#footer").before($(row).append(
-            emptyCol,
-            makeCard(products),
-            emptyCol
-        ))
-    }
+  for (let n = 0; n < lines; n++) {
+    $('#footer').before($(row).append(emptyCol, makeCard(products), emptyCol));
+  }
 }
+let imge= '<img src="" class="card-img-top">';
 
 function makeCard(products) {
-    let emptyCol = '<div class="col-2"></div>';
-    let card = '<div class="card"><div>';
-    let img = '<img src="" class="card-img-top">';
-    let cardBody = '<div class="card-body"></div>';
-    let cardTitle = '<h5 class="card-title">Apple</h5>';
-    let cardText = '<p class="card-text">6,98</p>';
-    let cardProduct = [];
-    for (let i = 0; i <4; i++) {
-        if (products.length > 0) {
-            cardProduct[i] = $(emptyCol).append(
-                $(card).append(
-                    $(img).attr("src",products[0].img).data("idProduct",products[0].id).click(showProduct),
-                    $(cardBody).append(
-                        $(cardTitle).text(products[0].title),
-                        $(cardText).text(products[0].price)
-                        )
-                        )
-                        )
-                    products.shift();
-        }
+  let emptyCol = '<div class="col-2"></div>';
+  let card = '<div class="card"><div>';
+  let img = '<img src="" class="card-img-top">';
+  let cardBody = '<div class="card-body"></div>';
+  let cardTitle = '<h5 class="card-title">Apple</h5>';
+  let cardText = '<p class="card-text">6,98</p>';
+  let cardProduct = [];
+  for (let i = 0; i < 4; i++) {
+    if (products.length > 0) {
+      cardProduct[i] = $(emptyCol).append(
+        $(card).append(
+          $(img)
+            .attr('src', products[0].img)
+            .data('idProduct', products[0].id)
+            .click(showProduct),
+          $(cardBody).append(
+            $(cardTitle).text(products[0].title),
+            $(cardText).text(products[0].price)
+          )
+        )
+      );
+      products.shift();
     }
-    return cardProduct;
+  }
+  return cardProduct;
 }
 
 function showProduct() {
-    alert($(event.target).data("idProduct"));
+  alert($(event.target).data('idProduct'));
+
 }
+
+// event listenr for card
+$('#dropdownMenuButton').on('click', (e) => {
+  console.log(e.target);
+  addToCart();
+});
+
+const addToCart = (food) => {
+  $('.cart-items').append("<img id='theImg' src='./src/img/watermelon.jpg'/>");
+
+};
