2d38dca0768cf688ff631921107404e223cf99ff
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-06-18 13:58:04 +0200

    completed login functionality

--- src/js/admin_script.js
+++ src/js/admin_script.js
@@ -1,3 +1,25 @@
+// GLOBAL VARIABLES
+/* Generate "shop" object for the first time,
+storing on it a master user to be able to log in */
+let shop = JSON.parse(localStorage.getItem("shopJSON"));
+if (!shop) {
+  shop = {
+    products: [],
+    categories: [],
+    admins: []
+  };
+  let masterAdmin = {
+    id: 1,
+    name: "admin",
+    surname: "administrator",
+    email: "master",
+    password: "1234"
+  }
+  shop.admins.push(masterAdmin);
+  localStorage.setItem("shopJSON", JSON.stringify(shop));
+}
+
+// EVENT LISTENERS
 $(document).ready(function () {
   // Event listener to expand/collapse sidebar
   $('#sidebarCollapse').on('click', function () {
@@ -9,9 +31,10 @@ $(document).ready(function () {
   forms[0].addEventListener('submit', function (event) {
     event.preventDefault();
     event.stopPropagation();
-    let isValid = validateLogin();
-    if (isValid) {
+    let adminName = validateLogin();
+    if (adminName != "") {
       setTimeout(function () {
+        $('.sidebar-header h3').text('Welcome ' + adminName + "!");
         $('.main-content').hide();
         $('.wrapper').css('display', 'flex');
       }, 1000)
@@ -19,25 +42,41 @@ $(document).ready(function () {
   });
 });
 
+
+// AUXILIAR FUNCTIONS
 function validateLogin() {
-  let validated = true;
+  let adminName = "";
+
+  let emailFound = false;
   let email = document.getElementById('email');
   let emailValue = email.value.trim();
-  if (emailValue === '') {
-    email.classList.add('is-invalid');
-    validated = false;
-  } else {
-    email.classList.remove('is-invalid');
-    email.classList.add('is-valid');
-  }
+
+  let passwordFound = false;
   let password = document.getElementById('password');
   let passwordValue = password.value.trim();
-  if (passwordValue === '') {
-    password.classList.add('is-invalid');
-    validated = false;
+
+  let shop = JSON.parse(localStorage.getItem("shopJSON"));
+  shop.admins.forEach(admin => {
+    if (admin.email == emailValue) {
+      emailFound = true;
+      if (admin.password == passwordValue) {
+        passwordFound = true;
+        adminName = admin.name;
+      }
+    }
+  })
+
+  if (emailFound) {
+    email.classList.remove('is-invalid');
+    email.classList.add('is-valid');
   } else {
+    email.classList.add('is-invalid');
+  }
+  if (passwordFound) {
     password.classList.remove('is-invalid');
     password.classList.add('is-valid');
+  } else {
+    password.classList.add('is-invalid');
   }
-  return validated;
+  return adminName;
 }
